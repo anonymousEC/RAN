3f3ab7aa2a1fa2e8eb55a7c6dd7d25ae10f5a61b
ych <canghaiyang@cqu.edu.cn>
2024-08-03 21:44:14 +0800

    1

--- script/RAN_HDFS_install.sh
+++ script/RAN_HDFS_install.sh
@@ -16,6 +16,7 @@ BASE_DIR="/home/ecRepair"
 SCRIPT_DIR="${BASE_DIR}/RAN/script"
 TOOLS_DIR="${BASE_DIR}/RAN/tools/dfs_tools_scripts"
 HADOOP_SRC_DIR="${BASE_DIR}/hadoop-3.1.4-src"
+CURRENT_DIR=$(pwd)
 
 # Configure environment variables and hostname
 cp ${TOOLS_DIR}/etc/* /etc/
@@ -24,13 +25,17 @@ ${SCRIPT_DIR}/scp_same.sh ${START_NODE} ${END_NODE} /etc/profile
 ${SCRIPT_DIR}/same_command.sh ${START_NODE} ${END_NODE} "source /etc/profile"
 source /etc/profile
 
-# Add RAN integration code to HDFS source code
-cp -r ${BASE_DIR}/RAN/HDFS_integrate/hadoop-common-project/ ${HADOOP_SRC_DIR}/
-cp -r ${BASE_DIR}/RAN/HDFS_integrate/hadoop-hdfs-project/ ${HADOOP_SRC_DIR}/
-
 # Install the required tools and libraries
 apt-get -y install build-essential autoconf automake libtool cmake zlib1g-dev pkg-config libssl-dev expect
 cd ${TOOLS_DIR}
 ${SCRIPT_DIR}/scp_same.sh ${START_NODE} ${END_NODE} protobuf-2.5.0.tar.gz
 ${SCRIPT_DIR}/same_command.sh ${START_NODE} ${END_NODE} "apt install -y openjdk-8-jdk maven"
 ${SCRIPT_DIR}/same_command.sh ${START_NODE} ${END_NODE} "cd ~ && tar -zxvf protobuf-2.5.0.tar.gz && cd protobuf-2.5.0 && sudo mkdir /usr/local/protoc-2.5.0/ && ./configure --prefix=/usr/local/protoc-2.5.0/ && make && sudo make install"
+
+
+# Add RAN integration code to HDFS source code
+cp -r ${BASE_DIR}/RAN/HDFS_integrate/hadoop-common-project/ ${HADOOP_SRC_DIR}/
+cp -r ${BASE_DIR}/RAN/HDFS_integrate/hadoop-hdfs-project/ ${HADOOP_SRC_DIR}/
+
+# Return to the original directory
+cd ${CURRENT_DIR}
