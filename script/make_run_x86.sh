6626bc0d42e1a48e6eef03017175aef4af41600d
ych <canghaiyang@cqu.edu.cn>
2024-08-03 12:27:17 +0800

    1

--- script/make_run_x86.sh
+++ script/make_run_x86.sh
@@ -3,5 +3,5 @@ make clean
 cmake . && make #PND is x86
 cd /home/ecRepair/RAN/script
 ./kill_all_storagenodes.sh $1 $2 && ./kill_all_pnetdevice.sh $3 && ./kill_client.sh #if have PND
-./scp_storagenodes.sh $1 $2 && ./scp_pnetdevice_arm.sh $3 #if have PND
+./scp_storagenodes.sh $1 $2 && ./scp_pnetdevice.sh $3 #if have PND
 ./start_all_storagenodes.sh $1 $2 && ./start_all_pnetdevice.sh $3  #if have PND
